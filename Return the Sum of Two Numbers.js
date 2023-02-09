f5ccb940df46cbeb9613e16187516c5f470f1cfd
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-02-09 19:43:20 +0600

    ok

--- Return the Sum of Two Numbers.js
+++ Return the Sum of Two Numbers.js
@@ -1,3 +1,10 @@
 //Create a function that takes two numbers as arguments and returns their sum
 //Don't forget to return the result.
 
+function Add(Num1,Num2) {
+    return Num1 + Num2;
+  }
+  //Call the function
+  var result = Add(10,20);
+  //Print the result
+  console.log(result);
