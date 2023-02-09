7da209aa532ceaee4793622682a5f022d4b71153
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-02-09 21:42:43 +0600

    ok

---
+++ Return the Next Number from the Integer Passed.js
@@ -0,0 +1,8 @@
+// Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function Next(n){
+    return n+1;
+}
+
+var result = Next(5);
+console.log(result);
