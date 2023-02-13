2aaa03f4a46a9f37b0cbaee92b8254be45f094c1
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-02-14 01:14:20 +0600

    ok

---
+++ Return the First Element in an Array.js
@@ -0,0 +1,11 @@
+// Create a function that takes an array containing only numbers and return the first element.
+
+// The first element in an array always has an index of 0.
+
+function returnFirstElement(array) { 
+    return array[0];
+ }
+
+ let arr = [1,2,3,4,5];
+ let firstCharacter = returnFirstElement(arr);
+ console.log(firstCharacter);
