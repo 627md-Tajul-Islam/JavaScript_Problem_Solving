14a110a1a2d777dda4df34bc017cfb6026df6a4b
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-02-14 01:57:02 +0600

    ok

---
+++ Return Something to Me.js
@@ -0,0 +1,7 @@
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+function joinString(a) {
+    return "something " + a;
+  }
+  var result = joinString("has joined");
+  console.log(result);
